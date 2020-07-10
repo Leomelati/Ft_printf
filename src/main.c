5f50ae7700956687ac61630fa3f5cde6e3c3163d
Lmartins <lmartins@42sp.org.br>
2020-07-10 05:34:17 +0200

    PCT (WIP 1)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 03:11:04 by lmartins          #+#    #+#             */
-/*   Updated: 2020/07/10 05:00:58 by lmartins         ###   ########.fr       */
+/*   Updated: 2020/07/10 05:20:50 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int main()
 	// int negative_int_test = -42;
 	// unsigned int unsigned_test = 4294967295u;
 
-	ft_printf("Printm: \"%% *.5i 42 == |% *.5i|\"\n", 4, 42);
-	printf("Printf: \"%% *.5i 42 == |% *.5i|\"\n", 4, 42);
+	ft_printf("Printm: \"%5%\"\n");
+	printf("Printf: \"%5%\"\n");
 	ft_printf("\n");
 	return (0);
 }
