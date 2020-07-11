ea3039f107f554c6fef55e2b2c9cce6f6d18d8b6
Lmartins <lmartins@42sp.org.br>
2020-07-11 05:21:10 +0200

    testing

--- src/hexadecimal.c
+++ src/hexadecimal.c
@@ -6,7 +6,7 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 08:04:16 by lmartins          #+#    #+#             */
-/*   Updated: 2020/07/11 05:18:54 by lmartins         ###   ########.fr       */
+/*   Updated: 2020/07/11 05:21:06 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,6 +114,7 @@ void	print_p_specifier(t_parameters *info, va_list ap)
 	ptr = (size_t)va_arg(ap, void *);
 	chartoprint = (info->zero == TRUE) ? '0' : ' ';
 	len = ft_hexlen(ptr) + 1;
+	spacestoprint = info->width - len - 2;
 	justify_padding(spacestoprint, chartoprint, info, FALSE);
 	adapted_putstr_fd("0x", 1, info);
 	if (!ptr)
