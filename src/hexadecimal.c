d4a2e807ec9e6cc3ae7e7dcb17db3846ee4691e1
Lmartins <lmartins@42sp.org.br>
2020-07-11 02:57:43 +0200

    testing p

--- src/hexadecimal.c
+++ src/hexadecimal.c
@@ -6,7 +6,7 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 08:04:16 by lmartins          #+#    #+#             */
-/*   Updated: 2020/07/11 02:56:22 by lmartins         ###   ########.fr       */
+/*   Updated: 2020/07/11 02:57:33 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,12 +117,10 @@ void	print_p_specifier(t_parameters *info, va_list ap)
 	if (ptr)
 		len = ft_hexlen(ptr) + 1;
 	else if ((!ptr) && (info->width == FALSE))
-		len = 3;
-	else if ((!ptr) && (info->precision != MISSING))
 		len = 2;
+	else if ((!ptr) && (info->precision != MISSING))
+		len = 3;
 	spacestoprint = (len >= info->width) ? 0 : (info->width - len);
-	if ((!ptr) && (info->precision == MISSING))
-		spacestoprint--;
 	justify_padding(spacestoprint, chartoprint, info, FALSE);
 	adapted_putstr_fd("0x", 1, info);
 	if ((!ptr) && info->precision == MISSING)
